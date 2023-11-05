fb375ce0b94476062e78ff1f3a0f5296440108be
Alex Regiani <alexregianideveloper@outlook.com>
2023-11-05 19:55:07 -0400

    created different states

--- lib/cubits/counter/counter_state.dart
+++ lib/cubits/counter/counter_state.dart
@@ -1,6 +1,23 @@
 part of 'counter_cubit.dart';
 
-@immutable
-abstract class CounterState {}
+class CounterState extends Equatable {
+  final int counter;
 
-class CounterInitial extends CounterState {}
+  CounterState({required this.counter});
+
+  factory CounterState.initial() {
+    return CounterState(counter: 0);
+  }
+
+  @override
+  List<Object> get props => [counter];
+
+  @override
+  String toString() {
+    return 'CounterState{counter: $counter}';
+  }
+
+  CounterState copyWith({int? counter}) {
+    return CounterState(counter: counter ?? this.counter);
+  }
+}
