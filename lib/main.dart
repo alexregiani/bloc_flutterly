11b577e132e501654f5b746cc8187e91a9ee15c4
Alex Regiani <alexregianideveloper@outlook.com>
2023-11-10 13:27:26 -0400

    more work

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:bloc_flutterly/cubits/counter/counter_cubit.dart';
-import 'package:bloc_flutterly/other_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:gap/gap.dart';
@@ -29,60 +28,44 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BlocListener<CounterCubit, CounterState>(
-      listener: (context, state) {
-        if (state.counter == 4) {
-          ScaffoldMessenger.of(context).showSnackBar(
-            const SnackBar(
-              content: Text('Congratulations 3'),
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Expanded(
+            child: BlocBuilder<CounterCubit, CounterState>(
+              builder: (context, state) {
+                return Center(
+                    child: Text(
+                  '${state.counter}',
+                  style: const TextStyle(fontSize: 100),
+                ));
+              },
             ),
-          );
-        } else if (state.counter == -1) {
-          Navigator.push(
-              context,
-              MaterialPageRoute(
-                builder: (context) => const OtherPage(),
-              ));
-        }
-      },
-      child: Scaffold(
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Expanded(
-              child: Center(child: BlocBuilder<CounterCubit, CounterState>(
-                builder: (context, state) {
-                  return Text(
-                    '${state.counter}',
-                    style: const TextStyle(fontSize: 100),
-                  );
-                },
-              )),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(15.0),
-              child: Align(
-                alignment: Alignment.bottomCenter,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
-                    FloatingActionButton(
-                      heroTag: 'decrement',
-                      onPressed: context.read<CounterCubit>().decrement,
-                      child: const Icon(Icons.exposure_minus_1),
-                    ),
-                    const Gap(10),
-                    FloatingActionButton(
-                      heroTag: 'increment',
-                      onPressed: BlocProvider.of<CounterCubit>(context).increment,
-                      child: const Icon(Icons.plus_one),
-                    ),
-                  ],
-                ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: Align(
+              alignment: Alignment.bottomCenter,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  FloatingActionButton(
+                    heroTag: 'decrement',
+                    onPressed: BlocProvider.of<CounterCubit>(context).decrement,
+                    child: const Icon(Icons.exposure_minus_1),
+                  ),
+                  const Gap(10),
+                  FloatingActionButton(
+                    heroTag: 'increment',
+                    onPressed: BlocProvider.of<CounterCubit>(context).increment,
+                    child: const Icon(Icons.plus_one),
+                  ),
+                ],
               ),
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
