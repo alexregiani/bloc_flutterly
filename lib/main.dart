362aaeb0b738f94eff03ad10d792f78afcba5489
Alex Regiani <alexregianideveloper@outlook.com>
2023-11-11 11:53:34 -0400

    basic UI

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,6 @@
-import 'package:bloc_flutterly/other_page.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:gap/gap.dart';
+import 'dart:math';
 
-import 'blocs/counter/counter_bloc.dart';
+import 'package:flutter/material.dart';
 
 //
 void main() {
@@ -17,11 +14,8 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BlocProvider(
-      create: (context) => CounterBloc(),
-      child: const MaterialApp(
-        home: HomePage(),
-      ),
+    return const MaterialApp(
+      home: HomePage(),
     );
   }
 }
@@ -32,60 +26,17 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: BlocListener<CounterBloc, CounterState>(
-        listener: (context, state) {
-          if (state.counter == 10) {
-            ScaffoldMessenger.of(context).showSnackBar(
-              const SnackBar(
-                content: Text('congratulations 10'),
-              ),
-            );
-          } else if (state.counter == 15) {
-            Navigator.push(context, MaterialPageRoute(
-              builder: (context) {
-                return const OtherPage();
-              },
-            ));
-          }
-        },
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Expanded(
-              child: Center(child: BlocBuilder<CounterBloc, CounterState>(
-                builder: (context, state) {
-                  return Text(
-                    '${state.counter}',
-                    style: const TextStyle(fontSize: 100),
-                  );
-                },
-              )),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(15.0),
-              child: Align(
-                alignment: Alignment.bottomCenter,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
-                    FloatingActionButton(
-                      heroTag: 'decrement',
-                      onPressed: () => BlocProvider.of<CounterBloc>(context).add(DecrementEvent()),
-                      child: const Icon(Icons.exposure_minus_1),
-                    ),
-                    const Gap(10),
-                    FloatingActionButton(
-                      heroTag: 'increment',
-                      onPressed: () {
-                        BlocProvider.of<CounterBloc>(context).add(IncrementEvent());
-                      },
-                      child: const Icon(Icons.plus_one),
-                    ),
-                  ],
-                ),
-              ),
-            ),
-          ],
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text('change theme app'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            final int randInt = Random().nextInt(10);
+            print('random number $randInt');
+          },
+          child: const Text(style: TextStyle(fontSize: 30), 'change theme'),
         ),
       ),
     );
