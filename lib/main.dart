0cb94214fd47d22498f63bdcc79de3f177471319
Alex Regiani <alexregianideveloper@outlook.com>
2023-11-19 21:15:23 -0400

    button now switches color

--- lib/main.dart
+++ lib/main.dart
@@ -1,86 +1,53 @@
-import 'package:bloc_flutterly/presentation/card_item.dart';
-import 'package:bloc_flutterly/presentation/item_detail.dart';
+import 'package:bloc_flutterly/cubit/color/color_cubit.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:gap/gap.dart';
 
-import 'bloc/item_bloc.dart';
-
 void main() {
   runApp(
-    BlocProvider(
-      create: (context) => ItemBloc(),
-      child: const MyApp(),
-    ),
+    const MyApp(),
   );
 }
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  @override
-  void initState() {
-    super.initState();
-    context.read<ItemBloc>().add(ItemFetchEvent());
-  }
-
-  @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-          appBar: AppBar(
-            title: const Text('Cubit'),
-          ),
-          body: BlocBuilder<ItemBloc, ItemState>(
-            builder: (context, state) {
-              if (state is ItemSuccessState) {
-                return ListView.builder(
-                  padding: const EdgeInsets.all(15),
-                  itemCount: state.items.length,
-                  itemBuilder: (context, index) {
-                    return GestureDetector(
-                      onTap: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => ItemDetail(
-                                title: state.items[index].title,
-                                price: state.items[index].price,
-                                description: state.items[index].description,
-                                image: state.items[index].image),
-                          ),
-                        );
-                      },
-                      child: Column(
-                        children: [
-                          CardItem(
-                            title: state.items[index].title,
-                            price: state.items[index].price,
-                            description: state.items[index].description,
-                            image: state.items[index].image,
-                          ),
-                          const Gap(10)
-                        ],
+    return BlocProvider(
+      create: (context) => ColorCubit(),
+      child: MaterialApp(
+        home: BlocBuilder<ColorCubit, ColorState>(
+          builder: (context, state) {
+            return Scaffold(
+                backgroundColor: state.color,
+                appBar: AppBar(
+                  title: const Text('Cubit'),
+                ),
+                body: Center(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      ElevatedButton(
+                        onPressed: () {
+                          BlocProvider.of<ColorCubit>(context).colorChange();
+                        },
+                        child: const Text(style: TextStyle(fontSize: 40), 'Change Color'),
                       ),
-                    );
-                  },
+                      const Gap(20),
+                      const Text(textScaleFactor: 3, 'Counter'),
+                      const Gap(20),
+                      ElevatedButton(
+                        onPressed: () {},
+                        child: const Text(style: TextStyle(fontSize: 40), 'Increment Counter'),
+                      )
+                    ],
+                  ),
+                ) // SizedBox(width: 200, height: 270, child: CardItem()),
                 );
-              } else if (state is ItemFailureState) {
-                return Text(state.error);
-              } else if (state is ItemInitialState) {
-                return const Center(child: CircularProgressIndicator());
-              } else {
-                return const Text('error');
-              }
-            },
-          )
-          // SizedBox(width: 200, height: 270, child: CardItem()),
-          ),
+          },
+        ),
+      ),
     );
   }
 }
