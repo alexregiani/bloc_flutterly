a5e3d69462be968096835da983a6d5cba487b654
Alex Regiani <alexregianideveloper@outlook.com>
2023-11-20 12:52:35 -0400

    working implementation

--- lib/cubit/counter/counter_cubit.dart
+++ lib/cubit/counter/counter_cubit.dart
@@ -1,38 +1,18 @@
-import 'dart:async';
-
 import 'package:bloc/bloc.dart';
-import 'package:equatable/equatable.dart';
-import 'package:flutter/material.dart';
-
-import '../color/color_cubit.dart';
 
 part 'counter_state.dart';
 
 class CounterCubit extends Cubit<CounterState> {
   int incrementSize = 1;
-  final ColorCubit colorCubit;
-  late final StreamSubscription colorSubscription;
 
-  CounterCubit({required this.incrementSize, required this.colorCubit}) : super(CounterState.initial()) {
-    colorSubscription = colorCubit.stream.listen((ColorState colorState) {
-      if (colorState.color == Colors.red) {
-        incrementSize = 1;
-      } else if (colorState.color == Colors.green) {
-        incrementSize = 10;
-      } else if (colorState.color == Colors.blue) {
-        incrementSize = 100;
-      }
-    });
-  }
+  CounterCubit() : super(CounterState.initial());
 
-  void counterIncrease() {
+  void counterIncrease(int incrementSize) {
     emit(state.copyWith(state.counter + incrementSize));
     print('${state.counter}');
   }
 
-  @override
-  Future<void> close() {
-    colorSubscription.cancel();
-    return super.close();
+  void reset() {
+    emit(state.copyWith(state.counter * 0));
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -11,8 +11,15 @@ void main() {
   );
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int incrementSize = 1;
 
   @override
   Widget build(BuildContext context) {
@@ -21,43 +28,59 @@ class MyApp extends StatelessWidget {
         BlocProvider(
           create: (context) => ColorCubit(),
         ),
-        BlocProvider(create: (context) => CounterCubit(colorCubit: context.read<ColorCubit>(), incrementSize: 1)),
+        BlocProvider(
+          create: (context) => CounterCubit(),
+        ),
       ],
       child: MaterialApp(
         home: BlocBuilder<ColorCubit, ColorState>(
           builder: (context, state) {
-            return Scaffold(
-                backgroundColor: state.color,
-                appBar: AppBar(
-                  title: const Text('Cubit'),
-                ),
-                body: Center(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      ElevatedButton(
-                        onPressed: () {
-                          BlocProvider.of<ColorCubit>(context).colorChange();
-                        },
-                        child: const Text(style: TextStyle(fontSize: 40), 'Change Color'),
-                      ),
-                      const Gap(20),
-                      BlocBuilder<CounterCubit, CounterState>(
-                        builder: (context, state) {
-                          return Text(textScaleFactor: 3, '${state.counter}');
-                        },
-                      ),
-                      const Gap(20),
-                      ElevatedButton(
-                        onPressed: () {
-                          BlocProvider.of<CounterCubit>(context).counterIncrease();
-                        },
-                        child: const Text(style: TextStyle(fontSize: 40), 'Increment Counter'),
-                      )
-                    ],
+            return BlocListener<ColorCubit, ColorState>(
+              listener: (context, state) {
+                if (state.color == Colors.red) {
+                  incrementSize = 1;
+                } else if (state.color == Colors.blue) {
+                  incrementSize = 10;
+                } else if (state.color == Colors.green) {
+                  incrementSize = 100;
+                }
+              },
+              child: Scaffold(
+                  backgroundColor: state.color,
+                  appBar: AppBar(
+                    title: const Text('Cubit'),
                   ),
-                ) // SizedBox(width: 200, height: 270, child: CardItem()),
-                );
+                  body: Center(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        ElevatedButton(
+                          onPressed: () {
+                            BlocProvider.of<ColorCubit>(context).colorChange();
+                          },
+                          child: const Text(style: TextStyle(fontSize: 40), 'Change Color'),
+                        ),
+                        const Gap(20),
+                        BlocBuilder<CounterCubit, CounterState>(
+                          builder: (context, state) {
+                            return Text(textScaleFactor: 3, '${state.counter}');
+                          },
+                        ),
+                        const Gap(20),
+                        ElevatedButton(
+                          onPressed: () => BlocProvider.of<CounterCubit>(context).counterIncrease(incrementSize),
+                          child: const Text(style: TextStyle(fontSize: 40), 'Increment Counter'),
+                        ),
+                        const Gap(20),
+                        ElevatedButton(
+                          onPressed: () => context.read<CounterCubit>().reset(),
+                          child: const Text('reset'),
+                        )
+                      ],
+                    ),
+                  ) // SizedBox(width: 200, height: 270, child: CardItem()),
+                  ),
+            );
           },
         ),
       ),
