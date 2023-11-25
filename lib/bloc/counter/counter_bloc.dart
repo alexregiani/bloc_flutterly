2d94d2ab06bb19df7f19c11c3cf641ba5438f3e8
Alex Regiani <alexregianideveloper@outlook.com>
2023-11-25 13:43:35 -0400

    transformer

--- lib/bloc/counter/counter_bloc.dart
+++ lib/bloc/counter/counter_bloc.dart
@@ -1,4 +1,5 @@
 import 'package:bloc/bloc.dart';
+import 'package:bloc_concurrency/bloc_concurrency.dart';
 import 'package:equatable/equatable.dart';
 
 part 'counter_event.dart';
@@ -6,13 +7,15 @@ part 'counter_state.dart';
 
 class CounterBloc extends Bloc<CounterEvent, CounterState> {
   CounterBloc() : super(CounterState.initial()) {
-    on<CounterAddEvent>((event, emit) {
-      // print('before change $state');
-      emit(state.copyWith(state.counter + 1));
-      // print('after change $state');
-    });
-    on<CounterSubtractEvent>((event, emit) {
+    on<CounterAddEvent>(_addCounter, transformer: droppable());
+    on<CounterSubtractEvent>((event, emit) async {
+      await Future.delayed(const Duration(seconds: 5));
       emit(state.copyWith(state.counter - 1));
     });
+  }
+
+  void _addCounter(event, emit) async {
+    await Future.delayed(const Duration(seconds: 3));
+    emit(state.copyWith(state.counter + 1));
   }
 }
