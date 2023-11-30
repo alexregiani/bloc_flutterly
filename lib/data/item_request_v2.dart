c8c08493099a36d3ac1b880b03346a2b6843e084
Alex Regiani <alexregianideveloper@outlook.com>
2023-11-29 20:05:29 -0400

    first version working

---
+++ lib/data/item_request_v2.dart
@@ -0,0 +1,37 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+
+class Product {
+  String title;
+  double price;
+  String description;
+
+  @override
+  String toString() {
+    return 'Product{title: $title, price: $price, description: $description}';
+  } // List<String> images;
+
+  Product({required this.title, required this.price, required this.description});
+
+  factory Product.fromJson(Map<String, dynamic> json) {
+    return Product(title: json['title'], price: json['price'].toDouble(), description: json['description']);
+  }
+}
+
+class NetworkRequest {
+  Future<List<Product>> getProducts() async {
+    final response = await http.get(Uri.parse('https://dummyjson.com/products'));
+    if (response.statusCode == 200) {
+      final json = jsonDecode(response.body);
+      final List<Product> result = [];
+      for (var i = 0; json.length > i; i++) {
+        var entry = Product.fromJson(json['products'][i]);
+        result.add(entry);
+      }
+      return result;
+    } else {
+      throw Exception('error');
+    }
+  }
+}
